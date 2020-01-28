1e4257f98778759c83885b4fbb5cd93cb9e29935
Irène Colonna <B00684776@essec.edu>
2020-01-28 14:46:59 +0100

    ajout exercice 2 suite

--- lib/player.rb
+++ lib/player.rb
@@ -39,6 +39,7 @@ class HumanPlayer < Player
 
 	def initialize(name)
 		super(name)
+		@life_points = 100
 		@weapon_level = 1
 	end
 
@@ -54,9 +55,32 @@ end
 
   def search_weapon
 	  new_weapon_level = rand(1..6)
-    if new_weapon_level > @weapon_level
+    puts "Tu as trouvé une arme de niveau #{new_weapon_level}"
+		if new_weapon_level > @weapon_level
 			@weapon_level = new_weapon_level
+		else
+			puts "M@*#$... elle n'est pas mieux que ton arme actuelle..."
 		end
 	end
 
-binding.pry
+	def search_health_pack
+	  health_level = rand(1..6)
+		if health_level == 1
+			puts "Tu n'as rien trouvé..."
+		elsif health_level > 1 && health_level < 6
+			puts "Bravo, tu as trouvé un pack de +50 points de vie !"
+			if @life_points + 50 <= 100
+				@life_points = @life_points + 50
+			else
+				@life_points = 100
+			end
+		else 
+			puts "Waow, tu as trouvé un pack de +80 points de vie !"
+			if @life_points + 80 <= 100
+				@life_points = @life_points + 80
+			else
+				@life_points = 100
+			end
+		end
+	end
+
